dd487fe6ab4aee02a97da5333192225db12dfade
Szymon Kuc <szymon.kuc@poczta.onet.pl>
2019-11-22 23:05:40 +0100

    modified:   package.json 	modified:   src/components/App.tsx 	deleted:    src/components/Postform.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 import '../styles/index.css';
-import Post from './Post';
-import PostForm from './Postform';
 import { Provider } from 'react-redux';
 import store from '../store'
 
@@ -10,8 +8,6 @@ const App:React.FC = () => {
 	return (
 		<>
 			<Provider store={store}>
-				<PostForm />
-				<Post />
 			</Provider>	
 		</>
 	);
