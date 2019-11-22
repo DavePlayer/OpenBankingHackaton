2844ff5170ea9e84127287bc170e02f4cf9179b2
Szymon Kuc <szymon.kuc@poczta.onet.pl>
2019-11-22 23:45:53 +0100

    added sass loader

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import '../styles/index.css';
+import '../styles/index.scss';
 import { Provider } from 'react-redux';
 import store from '../store'
 import Main from './Main';
