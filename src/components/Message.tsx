a6982b6c2b875bda11704b2f4860bc3946d27ef7
Szymon Kuc <szymon.kuc@poczta.onet.pl>
2019-11-23 01:07:26 +0100

    Added simple messages layout

---
+++ src/components/Message.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+
+const Message:React.FC = () => {
+	return (
+        <Grid container spacing={0} className="message">
+            <Grid item xs={2}>
+                <Avatar alt="user" className="message-avatar"/>
+            </Grid>
+            <Grid item xs={10}>
+                <Paper className="message-content">
+                    <h3>John Kowalski</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, quis?</p>
+                </Paper>
+            </Grid>
+        </Grid>
+	);
+}
+
+export default Message;
