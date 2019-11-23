5e1e5808185b2487b22c5b4f2cbcacd6c266b51b
DavePlayer <davepalyer2001@gmail.com>
2019-11-23 23:26:04 +0100

    flashback

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
-import { user, IUser } from './../reduceres/user',
+import { IUser } from './../reduceres/user';
 import { Paper, Typography } from '@material-ui/core';
 
 export const Profile = () => {
-    const user:IUser = useSelector( ( reducers:any ) => reducers.user )
+    const user:IUser = useSelector( ( reducers:any ) => reducers.user );
     return (
-        <Paper>
+        <Paper style={{textAlign: 'center'}}>
             <Typography variant='h4'>{`${user.name} ${user.lastName}`}</Typography>
             <Typography variant='h5'>{`mail: ${user.mail}`}</Typography>
         </Paper>
