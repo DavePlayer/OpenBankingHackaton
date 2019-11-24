d13cd6057707aaa3274bc9b29f267d50c271b37d
Szymon Kuc <szymon.kuc@poczta.onet.pl>
2019-11-24 09:58:15 +0100

    user

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -3,12 +3,15 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
+import { useSelector } from 'react-redux';
+import { IUser } from './../reduceres/user';
 
 interface IMessageProps {
     message: string;
 }
 
 const Message:React.FC<IMessageProps> = ({message}) => {
+    const user:IUser = useSelector( ( reducers:any ) => reducers.user );
 	return (
         <Grid container spacing={0} className="message">
             <Grid item xs={2}>
@@ -16,7 +19,7 @@ const Message:React.FC<IMessageProps> = ({message}) => {
             </Grid>
             <Grid item xs={10}>
                 <Paper className="message-content">
-                    <h3>John Kowalski</h3>
+                    <h3>{user}</h3>
                     <p>{message}</p>
                 </Paper>
             </Grid>
