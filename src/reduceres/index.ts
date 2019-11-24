cf266332956ece6a6b2857bb4b61464d40da61d4
Szymon Kuc <szymon.kuc@poczta.onet.pl>
2019-11-24 02:10:54 +0100

    added group

--- src/reduceres/index.ts
+++ src/reduceres/index.ts
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import sendMessage from './sendMessage';
 import goalReducer from './goalReducer';
 import { menu } from './menu'
+import groupReducer from './groupReducer';
 
 export default combineReducers({
     messages: sendMessage,
     goals: goalReducer,
+    groups: groupReducer,
     menu
 })
